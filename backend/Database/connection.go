a23bc2a71f6740dfd21bd09090a5c384f0d4c592
Sebastian <virez24@icloud.com>
2022-02-11 14:00:37 +0100

    Containerized version

--- backend/Database/connection.go
+++ backend/Database/connection.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	 "time"
 	models "github.com/sebastiankul-99/ToDo-App/Models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,10 +10,10 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	username := ""
-	password := ""
-	database := ""
-	connection, err := gorm.Open(mysql.Open(username+":"+password+"@/"+database), &gorm.Config{})
+
+	time.Sleep(4 * time.Second)
+	
+	connection, err := gorm.Open(mysql.Open("root:secretadmin@tcp(mysql-db-todo:3306)/todo_appdb?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic("Couldnt connect to database")
 	}
